97da08a1545a39314cf431859ae679982ba110a8
dSidy <snagatod74@gmail.com>
2026-01-13 23:22:59 +0000

    fix-bug : routes definition

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
 export const routes: Routes = [
-    {
-        path: '',
-        loadChildren: () => import('./features/home/home').then(m => m.Home)
-    }   
+  {
+    path: '',
+    component: Home,
+  },
 ];
