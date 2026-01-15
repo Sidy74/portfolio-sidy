1adc7bbc1743053104a8493c79fc53922e06051d
dSidy <snagatod74@gmail.com>
2026-01-15 16:24:48 +0000

    feat: hero section update & navigation on mobile

--- src/app/features/navbar/navbar.ts
+++ src/app/features/navbar/navbar.ts
@@ -1,16 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Briefcase, Github, Home, Linkedin, LucideAngularModule, Mail } from "lucide-angular";
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, LucideAngularModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  readonly github = Github;
+  readonly linkedin = Linkedin;
   menuItems = signal([
-    { label: 'Accueil', path: '/' },
-    { label: 'Projets', path: '/projets' },
-    { label: 'Contact', path: '/contact' },
+    { label: 'Accueil', path: '/',icon: Home  },
+    { label: 'Projets', path: '/projets', icon: Briefcase },
+    { label: 'Contact', path: '/contact', icon: Mail },
   ]);
 }
